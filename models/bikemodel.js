df6ce2bde1b0175c4f00fe02a48b063f0d48eba6
Tritia Timmins <tritiatimmins@Tritias-MacBook-Pro.local>
2016-03-05 17:01:51 -0800

    makes the users schema

---
+++ models/bikemodel.js
@@ -0,0 +1,6 @@
+//database url
+// mongodb://<dbuser>:<dbpassword>@ds019658.mlab.com:19658/biketrader
+
+//make our bike schema
+//make our bike model
+
---
+++ models/usermodel.js
@@ -0,0 +1,13 @@
+//userschema
+var mongoose = require('mongoose');
+
+var userSchema = new mongoose.schema({
+	id: Number,
+	name: String,
+	email: String,
+	phoneNumber: Number
+});
+
+var User = mongoose.model('User', userSchema)
+
+module.exports = User;
