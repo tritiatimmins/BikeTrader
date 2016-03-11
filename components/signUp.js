06794adaca275e260572d8a3424ad23a7f825d11
Dejon Gill <dejonjgill@gmail.com>
2016-03-10 16:50:36 -0800

    Got front end routes worked out, I think.

--- components/signUp.js
+++
@@ -1,3 +0,0 @@
-// angular.module('signUpController', []);
-
-console.log('hello');
---
+++ public/js/app.js
@@ -0,0 +1,23 @@
+angular.module('bikeTrader', ['ngRoute', 'HomeCtrl', 'PostCtrl'])
+.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+  $routeProvider
+    // nerds page that will use the NerdController
+    
+    .when('/', {
+      templateUrl: 'views/home.html',
+      controller: 'HomeController'
+    })
+
+    .when('/post', {
+      templateUrl: 'views/new-post.html',
+      controller: 'PostController'
+    });
+
+    // .when('*', {
+    //   templateUrl: '/views/new-post.html',
+    //   controller: 'PostController'
+    // });
+
+  // $locationProvider.html5Mode(true);
+
+}]);
