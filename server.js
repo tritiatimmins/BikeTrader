46359fa0eae964a4cf2a6b953e4296a26d89f0ac
DejonGill <dejonjgill@gmail.com>
2016-03-11 18:44:50 -0800

    POSTing and saving posts to the database!!!!!!!!!!!!!!!!!

--- server.js
+++ server.js
@@ -5,9 +5,10 @@ var path = require('path');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 var mongoose = require('mongoose');
+var Post = require('./app/models/post.js');
 
 // configure and connect to database
-var db = mongoose.connect('mongodb://tritiatimmins:Harley*8@ds019638.mlab.com:19638/tritiadb');
+var db = mongoose.connect('mongodb://tritiatimmins:Harley*8@ds019658.mlab.com:19658/biketrader');
 
 var port = process.env.PORT || 8080;
 
@@ -27,8 +28,20 @@ app.use(express.static(__dirname + '/public'));
 
 // routes will go here*************************************************
 // set up routes and require here
-app.post('/api/post', function(req, res) {
-  res.json(req.body);
+app.post('/api/post', function(req, res, next) {
+  
+  var newPost = new Post({
+    title: req.body.title,
+    description: req.body.description,
+    color: req.body.color,
+    price: req.body.price
+  });
+  newPost.save(function(err, newPost) {
+    if (err) { return next(err); }
+    res.json(req.body);
+  });
+  console.log('newPost', newPost);
+
 });
 
 // start app
